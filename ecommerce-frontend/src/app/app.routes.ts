c82484596b569c67329283a49d387c757999c935
Mohamed <mohamedtarekk710@gmail.com>
2026-05-29 03:56:47 +0300

    Fixed issue in sidenav + added home to routes and added navs to start with app itself(all pages)

--- ecommerce-frontend/src/app/app.routes.ts
+++ ecommerce-frontend/src/app/app.routes.ts
@@ -1,3 +1,8 @@
 import { Routes } from '@angular/router';
+import { HomePageComponent } from './components/home-page-component/home-page-component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: '', redirectTo: 'homepage', pathMatch: 'full'},
+
+    { path: 'homepage', component: HomePageComponent }
+];
--- ecommerce-frontend/src/app/app.ts
+++ ecommerce-frontend/src/app/app.ts
@@ -2,13 +2,17 @@ import { CommonModule } from '@angular/common';
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { SplashScreenComponent } from './components/splash-screen-component/splash-screen-component';
+import { SidenavComponent } from './components/sidenav-component/sidenav-component';
+import { NavbarComponent } from './components/topnav-component/topnav-component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, CommonModule, SplashScreenComponent],
+  standalone: true,
+  imports: [RouterOutlet, CommonModule, SplashScreenComponent, NavbarComponent, SidenavComponent],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
 export class App {
   showSplash = true;
+  sidenavOpen = false;
 }
