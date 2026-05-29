3b62e1fb3b27d92e9579aa1a64e1c6a8057ac9f7
Mohamed <mohamedtarekk710@gmail.com>
2026-05-29 03:50:58 +0300

    Created homepage that has childs component of sidenav,topnav,footer

--- ecommerce-frontend/src/app/components/home-page-component/home-page-component.ts
+++ ecommerce-frontend/src/app/components/home-page-component/home-page-component.ts
@@ -1,8 +1,9 @@
 import { Component } from '@angular/core';
+import { FooterSectionComponent } from '../footer-section-component/footer-section-component';
 
 @Component({
   selector: 'app-home-page-component',
-  imports: [],
+  imports: [FooterSectionComponent],
   templateUrl: './home-page-component.html',
   styleUrl: './home-page-component.css',
 })
