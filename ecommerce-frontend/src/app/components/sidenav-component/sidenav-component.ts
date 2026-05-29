d2d11d992001f0f3f46b93394ff776d8ac854799
Mohamed <mohamedtarekk710@gmail.com>
2026-05-29 03:28:00 +0300

    Added Sidenav burgermenu from top html css ts

--- ecommerce-frontend/src/app/components/sidenav-component/sidenav-component.ts
+++ ecommerce-frontend/src/app/components/sidenav-component/sidenav-component.ts
@@ -1,9 +1,25 @@
-import { Component } from '@angular/core';
+import { Component, Input, Output, EventEmitter } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import {
+  trigger, state, style, animate, transition
+} from '@angular/animations';
 
 @Component({
-  selector: 'app-sidenav-component',
-  imports: [],
+  selector: 'app-sidenav',
+  standalone: true,
+  imports: [CommonModule],
   templateUrl: './sidenav-component.html',
   styleUrl: './sidenav-component.css',
+  animations: [
+    trigger('slideIn', [
+      state('closed', style({ transform: 'translateX(-100%)' })),
+      state('open',   style({ transform: 'translateX(0)' })),
+      transition('closed => open', animate('400ms cubic-bezier(0.16, 1, 0.3, 1)')),
+      transition('open => closed', animate('300ms ease-in'))
+    ])
+  ]
 })
-export class SidenavComponent {}
+export class SidenavComponent {
+  @Input() isOpen = false;
+  @Output() close = new EventEmitter<void>();
+}
